20a03def6ce8174b39ef1e64387ec2d0338794ef
trainb0y <trainb0ydev@gmail.com>
2022-11-27 17:00:14 -0700

    Update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,10 @@
 pluginManagement {
     plugins {
         // Update this in libs.version.toml when you change it here
-        kotlin("jvm") version "1.7.10"
-        kotlin("plugin.serialization") version "1.7.10"
+        kotlin("jvm") version "1.7.21"
+        kotlin("plugin.serialization") version "1.7.21"
 
-        id("com.github.johnrengelman.shadow") version "7.1.0"
+        id("com.github.johnrengelman.shadow") version "7.1.2"
     }
 }
 
